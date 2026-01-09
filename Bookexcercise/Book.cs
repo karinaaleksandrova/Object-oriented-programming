7aa9f45869cb7a9276b562ad5f3eea1877afd4ca
Karina Aleksandrova <Karina.Aleksandrova@student.lab.fi>
2026-01-09 10:52:35 +0200

    update

---
+++ Bookexcercise/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookexcercise
+{
+    internal class Book
+    {
+            public string title;
+            public string author;
+            public double price;
+            public string id;
+
+            public Book()
+            {
+                this.title = string.Empty;
+                this.author = string.Empty;
+                this.price = 0;
+                this.id = string.Empty;
+
+            }
+
+            public Book(string title, string author, double price, string id)
+            {
+                this.title = title;
+                this.author = author;
+                this.price = price;
+                SetId(id); // this.id = id;
+            }
+
+            public void PrintBookInfo()
+            {
+                Console.WriteLine($"{this.author} : {this.title} : {this.price} : {this.id}");
+            }
+        public void SetId(string Idvalue)
+        {
+            if (Idvalue.Length == 5)
+            {
+                this.id = Idvalue;
+            }
+            else
+            {
+                this.id = "";
+            }
+
+             }
+        }
+    }
+
+
--- Bookexcercise/Program.cs
+++ Bookexcercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Bookexcercise
+﻿using Bookexcercise;
+
+namespace BookExercise
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("BookExercise");
+
+            Book abcBook = new Book("Aapinen", "M. Agricola", 10, "12312312312");
+            abcBook.PrintBookInfo();
+
+            Book mathBook = new Book();
+            mathBook.title = "Matikka 1 lk";
+            mathBook.author = "Matti Meikäläinen";
+            mathBook.PrintBookInfo();
         }
+
     }
 }

3aaf899be7b6876f3b16261eebefdef147d3f480
Karina Aleksandrova <Karina.Aleksandrova@student.lab.fi>
2026-01-14 10:56:58 +0200

    Valmis

---
+++ CompanyExercise/Company.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyExercise
+{
+    internal class Company
+    {
+        public string title;
+        public string address;
+        public string phone;
+        public double income;
+        public double expense;
+
+
+        //Copy constructor
+        public Company(Company otherCompany)
+        {
+            this.title = otherCompany.title;
+            this.address = otherCompany.address;
+            this.phone = otherCompany.phone;
+            this.income = otherCompany.income;
+            this.expense = otherCompany.expense;
+
+        }
+
+
+
+        public Company()
+        {
+            this.title = string.Empty;
+            this.address = string.Empty;
+            this.phone = string.Empty;
+            this.income = 0;
+            this.expense = 0;
+
+        }
+
+        public Company(string title, string address, string phone, double income, double expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phone = phone;
+            this.income = income;
+            this.expense = expense;
+        }
+
+        public override string? ToString()
+        {
+            return $"{this.title}, {this.address}, {this.phone}, {this.income}, {this.expense} "; //base.ToString();
+
+        }
+        public void CalculateProfit()
+        {
+            double profitProcent = (this.income - this.expense) / this.income * 100;
+
+
+            if (profitProcent < 5) 
+            {
+                Console.WriteLine("Yrityksellä menee huonosti.");
+            }
+
+            else if (profitProcent >= 5 && profitProcent <= 10)
+                {
+                    Console.WriteLine("Yrityksellä menee välttävästi");
+                }
+                else if (profitProcent >= 10 && profitProcent <= 20)
+                {
+                    Console.WriteLine("Yrityksellä menee tyydyttävästi");
+                }
+                else
+                {
+                    Console.WriteLine("Yrityksellä menee hyvin");
+                }
+
+            }
+        }
+    }
+
+
+
---
+++ CompanyExercise/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CompanyExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n--- CompanyExample");
+            Company c1 = new Company("X Oy", "Koulukatu 12", "0442994248", 10000000, 999000);
+            Company c3 = new Company("Z Oy", "Rantakuja 45", "04543839485", 6500000, 834879483);
+            Company c2 = new Company(c1);
+            c1.CalculateProfit();
+            
+
+            Console.WriteLine(c1.ToString());
+            Console.WriteLine(c2.ToString());
+            Console.WriteLine(c3);
+
+            Company[] companies = new Company[2];
+            companies[0] = new Company("X Oy", "Koulukatu 12", "0442994248", 10000000, 999000);
+            companies[1] = new Company("Z Oy", "Rantakuja 45", "04543839485", 6500000, 834879483);
+
+            Console.WriteLine("Testing foreach");
+            foreach (Company currentCompany in companies)
+            {
+                Console.WriteLine(currentCompany.ToString());
+                currentCompany.CalculateProfit();
+            }
+            Console.WriteLine("Testing for");
+            for (int i=0; i<companies.Length; i++)
+            {
+                Console.WriteLine(companies[i].ToString());
+                companies[i].CalculateProfit();
+            }
+            
+        }
+    }
+}

d6c79cabb9a9822ef7a75e0101efedda875d0544
Karina Aleksandrova <Karina.Aleksandrova@student.lab.fi>
2026-01-20 08:41:41 +0200

    Valmis Employee harjoitus

--- Employee/Program.cs
+++ Employee/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+
+            employee[] employee = new employee[3];
+            employee[0] = new employee("Martti Pekkanen", 777, "Sihteeri", 3000);
+            employee[1] = new employee("Tiina Matikainen", 888, "Päälikkö", 55000);
+            employee[2] = new employee("Kalevi Puro", 333, "Koodaaja", 5000);
+
+            Console.WriteLine("--Työntekijän tiedot:\n");
+
+            foreach (employee currentemployee in employee)
+            {
+                currentemployee.PrintEmployeeInfo();
+            }
+            Console.WriteLine("\n\n-- Verrataan palkkoja:\n");
+
+            for (int i = 0; i < employee.Length; i++)
+            {
+                employee[i].CompareSalary(employee[(i + 1) % employee.Length]);
+            }
         }
     }
 }
--- Employee/employee.cs
+++ Employee/employee.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,57 @@ namespace Employee
 {
     internal class employee
     {
+        public string name;
+        public int id;
+        public string position;
+        public double salary;
+        public double biggerSalary;
+        private static int Counter = 1;
+
+        public employee()
+        {
+            this.name = string.Empty;
+            this.id = 1;
+            this.position = string.Empty;
+            this.salary = 0;
+        }
+
+
+        public employee(string name, int id, string position, double salary)
+        {
+            this.name = name;
+            this.id = id;
+            this.position = position;
+            this.salary = salary;
+
+        }
+        public void CompareSalary(employee employee)
+        {
+            if (this.salary > employee.salary)
+            {
+                Console.WriteLine($"{this.name} palkka {this.salary} euroa on suurempi kuin {employee.name} palkka {employee.salary} euroa");
+            }
+            else
+            {
+                if (this.salary < employee.salary)
+                {
+                    Console.WriteLine($"{this.name} ja {employee.name} palkat ovat samat");
+                }
+            }
+        }
+            public void PrintEmployeeInfo()
+        {
+            Console.WriteLine(this.ToString());
+        }
+        public override string? ToString()
+        {
+            return $"Työntekijän nimi: {this.name}\n" +
+                $"Työntekijän id: {this.id}\n" +
+                $"Työtehtävä: {this.position}\n" +
+                $"Palkkatulot: {this.salary}\n";
+        }
     }
+
 }
+
+
